98cd36d74947608751b290a96217b2a49491cbd4
giovani <43485102+giovaniGdy@users.noreply.github.com>
2023-10-31 10:43:18 -0300

    Att. 1.1 Change style.css Added error filter

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 function calcular() {
-    let numero = document.getElementById("numero_celulas").value;
-    let diametro = document.getElementById("numero_diametro").value;
-    let Comprimento = document.getElementById("numero_comprimento").value;
+  let numero = document.getElementById("numero_celulas").value;
+  let diametro = document.getElementById("numero_diametro").value;
+  let Comprimento = document.getElementById("numero_comprimento").value;
 
-    area_colmo = 3.14159265359 * Math.pow(diametro / 2, 2);
+  area_colmo = 3.14159265359 * Math.pow(diametro / 2, 2);
 
-    let vol_circulo = numero * 100 * area_colmo;
-
-    let quant_colmo = vol_circulo * (Comprimento * 10000);
-
-    console.log(area_colmo);
-    console.log(vol_circulo);
-    console.log(quant_colmo);
-
-    document.getElementById("resultado").innerHTML = quant_colmo;
+  if(!numero) {
+    alert("Número de células não informado!")
   }
+  if (!diametro) {
+    alert("Diâmetro não informado!")
+  }
+  if (numero > 0 && diametro > 0) {
+    let vol_circulo = numero * 100 * area_colmo;
+    document.getElementById("resultado_colmo").innerHTML =
+      "Quant. em um colmo de " + diametro + " cm<sup>2</sup> = " + vol_circulo;
+    if (!Comprimento) {
+      document.getElementById("resultado").innerHTML = vol_circulo;
+    } else {
+      let quant_colmo = vol_circulo * (Comprimento * 10000);
+      document.getElementById("resultado_comprimento_colmo").innerHTML =
+        "Quant. em um colmo de " +
+        Comprimento +
+        "cm de altura = " +
+        quant_colmo;
+
+      document.getElementById("resultado").innerHTML = quant_colmo;
+    }
+  }
+}
