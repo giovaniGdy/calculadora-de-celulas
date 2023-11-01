b4c0d2644cdc0cd322fc3e0527a76ef1404a4f73
giovani <43485102+giovaniGdy@users.noreply.github.com>
2023-11-01 15:25:42 -0300

    Att 1.8 Fix style.css Added full form inputs

--- script.js
+++ script.js
@@ -2,37 +2,70 @@ function calcular() {
   let numero = document.getElementById("numero_celulas").value;
   let diametro = document.getElementById("numero_diametro").value;
   let Comprimento = document.getElementById("numero_comprimento").value;
+  let n_entrenos = document.getElementById("numero_entreno").value;
+  let n_perfilho = document.getElementById("numero_perfilho").value;
+  let valor_area = document.getElementById("numero_area").value;
 
   area_colmo = 3.14159265359 * Math.pow(diametro / 2, 2);
 
-  if(!numero) {
-    alert("Número de células não informado!")
+  if (!numero) {
+    alert("Número de células não informado!");
   }
-  
+
   if (!diametro) {
-    alert("Diâmetro não informado!")
+    alert("Diâmetro não informado!");
   }
 
   if (numero > 0 && diametro > 0) {
     let vol_circulo = numero * 100 * area_colmo;
-    let vol_circulo_formated = vol_circulo.toLocaleString('brl')
+    let vol_circulo_formated = vol_circulo.toLocaleString("brl");
 
     document.getElementById("resultado_colmo").innerHTML =
-      "Quant. em um colmo de " + parseFloat(area_colmo).toFixed(3) + " cm<sup>2</sup> = " + vol_circulo_formated;
+      "Quant. em um colmo de " +
+      parseFloat(area_colmo).toFixed(3) +
+      " cm<sup>2</sup> = " +
+      vol_circulo_formated;
 
     if (!Comprimento) {
       document.getElementById("resultado").innerHTML = vol_circulo_formated;
     } else {
       let quant_colmo = vol_circulo * (Comprimento * 10000);
-      
-      let quant_colmo_formated = quant_colmo.toLocaleString('brl')
+
+      let quant_colmo_formated = quant_colmo.toLocaleString("brl");
       document.getElementById("resultado_comprimento_colmo").innerHTML =
-        "Quant. em um colmo de " +
+        "Quant. em um entrenó de " +
         Comprimento +
         "cm de altura = " +
         quant_colmo_formated;
 
-      document.getElementById("resultado").innerHTML = quant_colmo_formated;
+      let quant_n_colmo = quant_colmo * n_entrenos;
+
+      let quant_n_colmo_formated = quant_n_colmo.toLocaleString("brl");
+      document.getElementById("resultado_numero_entreno").innerHTML =
+        "Quant. em um colmo de " +
+        n_entrenos +
+        " entrenós = " +
+        quant_n_colmo_formated;
+
+      let quant_n_perfilho = quant_n_colmo * n_perfilho;
+
+      let quant_n_perfilho_formated = quant_n_perfilho.toLocaleString("brl");
+      document.getElementById("resultado_numero_perfilho").innerHTML =
+        "Quant. em uma planta com " +
+        n_perfilho +
+        " perfilhos = " +
+        quant_n_perfilho_formated;
+
+      let quant_area = (valor_area/0.45)*4*quant_n_perfilho;
+
+      let quant_area_formated = quant_area.toLocaleString("brl");
+      document.getElementById("resultado_area").innerHTML =
+        "Quant. em uma área de " +
+        valor_area +
+        " m<sup>2</sup> = " +
+        quant_area_formated;
+
+      document.getElementById("resultado").innerHTML = quant_area_formated;
     }
   }
 }
