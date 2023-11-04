68b4d047e0d6126be8c149c83c3ba0eb11ce50de
giovani <43485102+giovaniGdy@users.noreply.github.com>
2023-11-03 23:10:49 -0300

    Att 2.2 Fix style.css Finished script formulas Added some information on home page

--- script.js
+++ script.js
@@ -28,46 +28,62 @@ function calcular() {
       " cm<sup>2</sup> = " +
       vol_circulo_formated;
 
-    let quant_colmo = vol_circulo * (Comprimento * 10000);
+    document.getElementById("resultado").innerHTML = vol_circulo_formated;
 
-    let quant_colmo_formated = quant_colmo.toLocaleString("brl");
-    document.getElementById("resultado_comprimento_colmo").innerHTML =
-      "Quant. em um entrenó de " +
-      Comprimento +
-      " cm de altura = " +
-      quant_colmo_formated;
+    if (Comprimento > 0) {
+      var quant_colmo = vol_circulo * (Comprimento * 10000);
 
-    let quant_n_colmo = quant_colmo * n_entrenos;
+      let quant_colmo_formated = quant_colmo.toLocaleString("brl");
+      document.getElementById("resultado_comprimento_colmo").innerHTML =
+        "Quant. em um entrenó de " +
+        Comprimento +
+        " cm de altura = " +
+        quant_colmo_formated;
 
-    let quant_n_colmo_formated = quant_n_colmo.toLocaleString("brl");
-    document.getElementById("resultado_numero_entreno").innerHTML =
-      "Quant. em um colmo com " +
-      n_entrenos +
-      " entrenós = " +
-      quant_n_colmo_formated;
+      document.getElementById("resultado").innerHTML = quant_colmo_formated;
 
-    let quant_n_perfilho = quant_n_colmo * n_perfilho;
+      if (n_entrenos > 0) {
+        var quant_n_colmo = quant_colmo * n_entrenos;
 
-    let quant_n_perfilho_formated = quant_n_perfilho.toLocaleString("brl");
-    document.getElementById("resultado_numero_perfilho").innerHTML =
-      "Quant. em uma planta com " +
-      n_perfilho +
-      " perfilhos = " +
-      quant_n_perfilho_formated;
+        let quant_n_colmo_formated = quant_n_colmo.toLocaleString("brl");
+        document.getElementById("resultado_numero_entreno").innerHTML =
+          "Quant. em um colmo com " +
+          n_entrenos +
+          " entrenós = " +
+          quant_n_colmo_formated;
 
-    let quant_area = Math.floor(valor_area / 1.5 / 0.3) * quant_n_perfilho;
+        document.getElementById("resultado").innerHTML = quant_n_colmo_formated;
 
-    let quant_area_formated = quant_area.toLocaleString("brl");
-    document.getElementById("resultado_area").innerHTML =
-      "Quant. em uma área de " +
-      valor_area +
-      " m<sup>2</sup> = " +
-      quant_area_formated;
+        if (n_perfilho > 0) {
+          var quant_n_perfilho = quant_n_colmo * n_perfilho;
 
-    let arr = [quant_colmo, quant_n_colmo, quant_n_perfilho, quant_area];
+          let quant_n_perfilho_formated =
+            quant_n_perfilho.toLocaleString("brl");
+          document.getElementById("resultado_numero_perfilho").innerHTML =
+            "Quant. em uma planta com " +
+            n_perfilho +
+            " perfilhos = " +
+            quant_n_perfilho_formated;
 
-    document.getElementById("resultado").innerHTML = Math.max(
-      ...arr
-    ).toLocaleString("brl");
+          document.getElementById("resultado").innerHTML =
+            quant_n_perfilho_formated;
+
+          if (valor_area > 0) {
+            var quant_area =
+              Math.floor(valor_area / 1.5 / 0.3) * quant_n_perfilho;
+
+            let quant_area_formated = quant_area.toLocaleString("brl");
+            document.getElementById("resultado_area").innerHTML =
+              "Quant. em uma área de " +
+              valor_area +
+              " m<sup>2</sup> = " +
+              quant_area_formated;
+
+            document.getElementById("resultado").innerHTML =
+              quant_area_formated;
+          }
+        }
+      }
+    }
   }
 }
